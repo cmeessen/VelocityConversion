597c2e1d5cd3c216effc984fad164a06f0710762
Christian Meeßen <christian.meessen@gfz-potsdam.de>
2017-11-15 14:52:58 +0100

    Added copyright and license disclaimer

--- Conversion.py
+++ Conversion.py
@@ -1,4 +1,20 @@
-# -*- coding: utf-8 -*-
+################################################################################
+#                     Copyright (C) 2017 by Christian Meeßen                   #
+#                                                                              #
+#                    This file is part of VelocityConversion                   #
+#                                                                              #
+#   VelocityConversion is free software: you can redistribute it and/or modify #
+#     it under the terms of the GNU General Public License as published by     #
+#            the Free Software Foundation version 3 of the License.            #
+#                                                                              #
+#   VelocityConversion is distributed in the hope that it will be useful, but  #
+#          WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       #
+#                   General Public License for more details.                   #
+#                                                                              #
+#      You should have received a copy of the GNU General Public License       #
+#   along with VelocityConversion. If not, see <http://www.gnu.org/licenses/>. #
+################################################################################
 """
 This code is a python implementation of the p- and s-wave velocity to density
 conversion approach after Goes et al. (2000)
